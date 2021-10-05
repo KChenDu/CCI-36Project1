69ca9182aa49279e82cbc5fd2296f3b2fcca9780
Mocardo <ricardomacedopacheco@gmail.com>
2021-10-04 23:06:32 -0300

    added gitignore and all planets

--- canvas.js
+++ canvas.js
@@ -47,22 +47,33 @@ function Orb(rad, color, ang_v, fat_dist=0, is_sun=false) {
     this.fat_dist = fat_dist
 }
 
-const sun = new Orb(2, 0xFF4500, 0, 0, true)
-const earth = new Orb(0.5, 0x000080, 0.01, 10)
-const moon = new Orb(0.2, 0xd3d3d3, 0.13, 1.5)
-const venus = new Orb(0.5, 0xdaa520, 0.015, 7)
-const mercury = new Orb(0.6, 0x696969, 0.045, 4)
-
-scene.add(sun.m);
-scene.add(venus.m);
-scene.add(mercury.m);
-earth.m.add(moon.m)
-scene.add(earth.m)
-
 const sunshine = new THREE.PointLight(0xffffff, 3.0);
 scene.add(sunshine);
 
-orbs = [sun, earth, moon, venus, mercury]
+const sun     = new Orb(  1, 0xFF4500,     0,   0, true)
+const mercury = new Orb(0.6, 0x696969, 0.045,   4)
+const venus   = new Orb(0.5, 0xdaa520, 0.015,   5)
+const earth   = new Orb(0.5, 0x000080,  0.01,   7)
+const mars    = new Orb(0.5, 0xFF0000, 0.010,  10)
+const jupiter = new Orb(0.8, 0x00FF00, 0.005,  12)
+const saturn  = new Orb(0.5, 0xFFAA00, 0.008,  13)
+const uranus  = new Orb(0.5, 0x00FFFF, 0.003,  14)
+const neptune = new Orb(0.5, 0x00FFFF, 0.001,  15)
+
+const planets = [mercury, venus, earth, mars, jupiter, saturn, uranus, neptune]
+
+scene.add(sun.m)
+for (let p of planets) {
+    scene.add(p.m)
+}
+
+const moon    = new Orb(0.2, 0xd3d3d3,  0.13, 1.5)
+earth.m.add(moon.m)
+
+const others = [moon]
+
+const orbs = [...[sun], ...planets, ...others]
+console.log(orbs)
 
 const animate = function () {
     // controls.update();
